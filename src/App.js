e3208be19ddf715f8c1e6f8d7e5a6721e1e5cedb
squashgray <squashgray@gmail.com>
2019-11-04 17:06:57 -0800

    cheap stretch

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHome] = useState(0);
   const [awayScore, setAway] = useState(0);
+  
 
   return (
     <div className="container">
@@ -37,6 +39,8 @@ function App() {
           <button onClick={() => setAway(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={() => setAway(homeScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
+        
+        
       </section>
     </div>
   );
